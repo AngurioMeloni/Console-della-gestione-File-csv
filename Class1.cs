d7e20f03dfe301cfccc03f77bb22bc825c00e5f1
Riccardo Masserini <masserini.riccardo.studente@itispaleocapa.it>
2023-09-13 19:52:30 +0200

    implementazione di tutte le funzioni

---
+++ Class1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_della_gestione_File_csv
+{
+    internal class Class1
+    {
+        public static Random r = new Random();
+        public static int Istruzione1()
+        {
+            string s;
+            int i = 0;
+            int giusto = 0;
+            StreamWriter writer = new StreamWriter("masserini1.csv", append: false);
+
+
+            if (File.Exists("masserini.csv"))
+            {
+                StreamReader reader = new StreamReader("masserini.csv");
+                s = reader.ReadLine();
+                while (s != null)
+                {
+                    if (i == 0)
+                    {
+                        writer.WriteLine(s + ";Valore Randomico;Campo Cancellazione Logica;Campo Univoco");
+                    }
+                    else
+                    {
+                        int valore = r.Next(10, 21);
+                        writer.WriteLine(s + ";" + valore + ";false;" + i + "");
+                    }
+                    i++;
+                    s = reader.ReadLine();
+                }
+                reader.Close();
+            }
+            else
+            {
+                giusto = 1;
+            }
+            writer.Close();
+
+            return giusto;
+        }
+
+        public static int Istruzione2()
+        {
+            string s;
+            int contatore;
+            StreamReader reader = new StreamReader("masserini1.csv");
+            s = reader.ReadLine();
+            reader.Close();
+            return contatore = s.Split(';').Length;
+
+        }
+
+        public static int Istruzione3()
+        {
+            StreamReader reader = new StreamReader("masserini1.csv");
+            int lunghezzaStringa = 0, lunghezzaMax = 0, i = 0;
+            string s;
+
+            s = reader.ReadLine();
+            while (s != null)
+            {
+                lunghezzaStringa = s.Length;
+                if (i != 0)
+                {
+                    if (lunghezzaMax < lunghezzaStringa)
+                    {
+                        lunghezzaMax = s.Length;
+                    }
+                }
+                s = reader.ReadLine();
+                i++;
+            }
+            reader.Close();
+            return lunghezzaMax;
+        }
+
+        public static int[] Istruzione3a()
+        {
+            StreamReader reader = new StreamReader("masserini1.csv");
+            string s = reader.ReadLine();
+            int[] lunghezzaMax = new int[Istruzione2()];
+            int asd = 0;
+            s = reader.ReadLine();
+
+            while (s != null)
+            {
+                string[] split = s.Split(';');
+                string[] array = new string[Istruzione2()];
+
+                for (int i = 0; i < Istruzione2(); i++)
+                {
+                    reader.DiscardBufferedData();
+                    reader.BaseStream.Seek(0, System.IO.SeekOrigin.Begin);
+                    s = reader.ReadLine();
+                    asd = 0;
+                    while (s != null)
+                    {
+                        string[] stringaSplit = s.Split(';');
+                        if (asd != 0)
+                        {
+                            if (lunghezzaMax[i] < stringaSplit[i].Length)
+                            {
+                                lunghezzaMax[i] = stringaSplit[i].Length;
+                            }
+
+                        }
+                        s = reader.ReadLine();
+                        asd++;
+                    }
+                }
+            }
+            reader.Close();
+            return lunghezzaMax;
+        }
+
+        public static void Istruzione5(string anno, string nazioni, string MKwh, string note, string Vrandom, string Vbooleano)
+        {
+            int i = 0;
+            StreamReader reader = new StreamReader("masserini1.csv");
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                i++;
+                line = reader.ReadLine();
+            }
+            reader.Close();
+            var oStream = new FileStream("masserini1.csv", FileMode.Append, FileAccess.Write, FileShare.Read);
+            BinaryWriter writer = new BinaryWriter(oStream);
+            string l = $"{anno};{nazioni};{MKwh};{note};{Vrandom};{Vbooleano};{i}".PadRight(200);
+            byte[] data = Encoding.ASCII.GetBytes(l);
+            writer.Write(data);
+
+            writer.Close();
+            oStream.Close();
+        }
+        public static void Istruzione4()
+        {
+            string s;
+            int i = 0;
+            StreamReader reader = new StreamReader("masserini1.csv");
+            StreamWriter writer = new StreamWriter("temporaneo.csv");
+
+            s = reader.ReadLine();
+            while (s != null)
+            {
+                if (i != 0)
+                {
+                    writer.WriteLine(s.PadRight(200));
+                }
+                else
+                {
+                    writer.WriteLine(s.PadRight(200));
+                }
+
+                s = reader.ReadLine();
+                i++;
+            }
+
+            reader.Close();
+            writer.Close();
+
+            File.Replace("temporaneo.csv", "masserini1.csv", "backup.csv");
+        }
+
+        public static int Istruzione7(string parola)
+        {
+            StreamReader reader = new StreamReader("masserini1.csv");
+            string s;
+            int i = 0;
+            s = reader.ReadLine();
+            while (s != null)
+            {
+
+                String[] split = s.Split(';');
+                String[] split1 = split[Istruzione2() - 1].Split(' ');
+
+                if (split1[5] == parola)
+                {
+                    reader.Close();
+                    return i;
+                }
+
+                i++;
+                s = reader.ReadLine();
+
+            }
+            reader.Close();
+            return -1;
+        }
+
+        public static void Istruzione8(string anno, string regione, string Mkwh, string note, string Vrandom, string Vbooleano, int linea)
+        {
+            var oStream = new FileStream("masserini1.csv", FileMode.Open, FileAccess.Write, FileShare.Read);
+            BinaryWriter writer = new BinaryWriter(oStream);
+
+            oStream.Seek(0, SeekOrigin.Begin);
+
+            oStream.Seek((200 * linea), SeekOrigin.Current);
+            string s = $"{anno};{regione};{Mkwh};{note};{Vrandom};{Vbooleano};{linea}".PadRight(200);
+            byte[] data = Encoding.ASCII.GetBytes(s);
+            writer.Write(data);
+
+            writer.Close();
+        }
+
+        public static int Istruzione9(string ricerca)
+        {
+            int riga = Istruzione7(ricerca);
+            int i = 0;
+            int successo = 0;
+            var readStream = new FileStream("masserini1.csv", FileMode.Open, FileAccess.Read, FileShare.Read);
+            BinaryReader read = new BinaryReader(readStream);
+
+            //Legge i dati e li converte in stringa
+            readStream.Seek(0, SeekOrigin.Begin);
+            readStream.Seek((200 * riga), SeekOrigin.Current);
+
+            byte[] data = new byte[200];
+            readStream.Read(data, 0, 200);
+            string s = Encoding.ASCII.GetString(data);
+
+            readStream.Close();
+            read.Close();
+
+            String[] split = s.Split(';');
+            String[] split1 = split[7].Split(' ');
+
+            var writeStream = new FileStream("masserini1.csv", FileMode.Open, FileAccess.Write, FileShare.Write);
+            BinaryWriter writer = new BinaryWriter(writeStream);
+
+            writeStream.Seek(0, SeekOrigin.Begin);
+            writeStream.Seek((200 * riga), SeekOrigin.Current);
+
+            //Scrive i dati
+            string linea = $"{split[0]};{split[1]};{split[2]};{split[3]};{split[4]};{split[5]};true;{split[7]}".PadRight(200);
+            byte[] data2 = Encoding.ASCII.GetBytes(linea);
+            writer.Write(data2);
+            successo = 1;
+
+            writer.Close();
+            writeStream.Close();
+
+            return successo;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,152 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Console_della_gestione_File_csv;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Elaborazione_dati_CSV__ripasso_app_cosnole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c = new Class1();
+            int fine = -1;
+            while (fine != 0)
+            {
+                Console.Clear();
+                Console.Write("Opzioni disponibili: ");
+                Console.WriteLine("\n\t0 - Esci dal programma\n\t1 - Aggiungi un valore casuale alla fine di ogni record\n\t2 - Contare il numero di campi che compongono un record\n\t3 - Calcolare la lunghezza massima dei record\n\t4 - Calcola la lunghezza massima dei record \n\t5 - Inserire un numero di spazi per fissare e rendere uguale la lunghezza di tutti i record\n\t6 - Aggiungere un record\n\t7 - Mostrare dei dati scegliendo tre campi\n\t8 - Cercare un record attraverso l'inserimento di un campo chiave\n\t9 - Modificare un record\n\t10 - Cancellare logicamente un record");
+                Console.Write("Scegli una opzione: ");
+                fine = int.Parse(Console.ReadLine());
+                switch(fine)
+                {
+                    case 1:
+                        Class1.Istruzione1();
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        Console.SetCursorPosition(48, 12);
+                        Console.WriteLine("Il numero di campi presenti nel file CSV sono " + Class1.Istruzione2());
+                        Thread.Sleep(2000);
+                        break;
+
+                    case 3:
+                        Console.Clear();
+                        Console.SetCursorPosition(48, 12);
+                        Console.WriteLine("Il campo più lungo ha " + Class1.Istruzione3() + " caratteri");
+                        Thread.Sleep(2000);
+                        break;
+
+                    case 4:
+                        Console.Clear(); // pulizia della console
+                        int contatore = 9;
+                        int[] MaxC = Class1.Istruzione3a();//pongo l'array uguale alla funzione Istruzione3A
+                        for (int k = 0; k < contatore; k++)//ciclo for che mi permette di visualizzare i valori dell'array nella listview
+                        {
+                            Console.WriteLine(MaxC[k].ToString());//visualizzazione dei valori dell'array nella listview
+                        }
+                        break;
+                  
+                    case 5:
+                        Class1.Istruzione4();
+                        break;
+
+                    case 6:
+                        Console.Clear();
+                        string[] New = new string[10];
+                        Console.Write("Inserire la l'anno: ");
+                        New[0] = Console.ReadLine();
+                        Console.Write("inserire la nazione: ");
+                        New[1] = Console.ReadLine();
+                        Console.Write("Inserire i milioni di kw/ora : ");
+                        New[2] = Console.ReadLine();
+                        Console.Write("Inserire le note: ");
+                        New[3] = Console.ReadLine();
+                        Console.Write("Inserire un valore casuale tra 1 e 20: ");
+                        New[4] = Console.ReadLine();
+                        Console.Write("Inserire true o false: ");
+                        New[5] = Console.ReadLine();
+                        Class1.Istruzione5(New[0], New[1], New[2], New[3], New[4], New[5]);
+                        break;
+
+                    case 7:
+                        Console.Clear();
+                        int i = 0;//dichiaro la variabile i
+                        StreamReader reader = new StreamReader("masserini1.csv");//dichiaro lo streamreader
+                        string n = reader.ReadLine();//pongo la variabile n uguale alla lettura della prima riga del file
+                        while (n != null)//ciclo while che mi permette di leggere tutte le righe del file
+                        {
+                            String[] split2 = n.Split(';');//dichiaro un array di stringhe
+                            String[] split3 = split2[5].Split(' ');//dichiaro un array di stringhe
+                            if (split3[0] == "false")//se split1 è uguale a false
+                            {
+                                Console.WriteLine(split2[0] + ';' + split2[1] + ';' + split2[2]);//aggiungo alla console i 3 campi
+                            }
+                            i++;//incremento i
+                            n = reader.ReadLine();//leggo la riga successiva
+                        }
+                        reader.Close();//chiudo lo streamreader
+                        break;
+
+                    case 8:
+                        Console.Clear();
+                        Console.Write("Inserisci una nazione da ricercare: ");
+                        string t = Console.ReadLine();
+                        int j = Class1.Istruzione7(t);
+                        if (j != -1)
+                        {
+                            Console.Clear();
+                            Console.SetCursorPosition(30, 12);
+                            Console.WriteLine("Elemento trovato alla riga" + j);
+                            Thread.Sleep(2000);
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.SetCursorPosition(30, 12);
+                            Console.WriteLine("Elemento non trovato");
+                            Thread.Sleep(2000);
+                        }
+                        break;
+
+                    case 9:
+                        Console.Clear();
+                        Console.Write("Inserisci la linea da modificare(utilizzare funzione 7 per trovare la linea): ");
+                        int l = int.Parse(Console.ReadLine());
+                        Console.Clear();
+                        Console.Clear();
+                        string[] mod = new string[10];
+                        Console.Write("Inserire l'anno: ");
+                        mod[0] = Console.ReadLine();
+                        Console.Write("Inserire la nazione: ");
+                        mod[1] = Console.ReadLine();
+                        Console.Write("Inserire i milioni di kw/ora: ");
+                        mod[2] = Console.ReadLine();
+                        Console.Write("Inserire le note: ");
+                        mod[3] = Console.ReadLine();
+                        Console.Write("Inserire il numero randomico da 1 a 20 : ");
+                        mod[4] = Console.ReadLine();
+                        Console.Write("Inserire true o false: ");
+                        mod[5] = Console.ReadLine();
+                        Class1.Istruzione8(mod[0], mod[1], mod[2], mod[3], mod[4], mod[5],l);
+                        break;
+
+                    case 10:
+                        Console.Clear();
+                        Console.Write("Inserisci la linea da cancellare(utilizzare funzione 7 per trovare la linea): ");
+                        string ricerca = Console.ReadLine();
+                        Class1.Istruzione9(ricerca);
+                        break;
+                }
+            }
+            Console.Clear();
+            Console.SetCursorPosition(30, 12);
+            Console.WriteLine("Fine del programma");
+            Thread.Sleep(10000);
+        }
+    }
+}
